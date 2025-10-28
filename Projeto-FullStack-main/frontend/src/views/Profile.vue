125c32932159e41191980eb0d1f96c0341c85948
Igor <igormatheusmarianodasilva@gmail.com>
2025-10-27 21:54:31 -0300

    inicio CRUD usuários

--- Projeto-FullStack-main/frontend/src/views/Profile.vue
+++ Projeto-FullStack-main/frontend/src/views/Profile.vue
@@ -17,7 +17,7 @@
         :initial="editing"
         :submitting="store.loading"
         @submit="(payload) => { store.updateMaquina(editing._id, payload); editing=null; }"
-        @cancel="cancelEdit"
+        @cancel="cancelEditM"
         edit
       />
     </div>
@@ -29,11 +29,48 @@
     <div class="bg-white rounded-2xl shadow-lg overflow-x-auto">
       <MaquinaList
         :maquina="store.maquinas"
+        @edit="editM"
+        @remove="store.removeMaquina"
+      />
+    </div>
+  </section>
+
+  <section class="min-h-[calc(100vh-132px)] p-10 flex flex-col max-w-6xl mx-auto">
+    <h2 class="text-3xl font-bold text-cyan-400 mb-8">
+      Usuários ({{ store.totalUsuarios }})
+    </h2>
+
+    <div class="flex flex-col bg-white rounded-2xl shadow-lg p-6 mb-10">
+
+      <UsuarioForm
+        v-if="!editing"
+        :submitting="store.loading"
+        @submit="store.addUsuario"
+      />
+      
+      <UsuarioForm
+        v-else
+        :initial="editing"
+        :submitting="store.loading"
+        @submit="(payload) => { store.updateUsuario(editing._id, payload); editing=null; }"
+        @cancel="cancelEditU"
+        edit
+      />
+    </div>
+
+    <p v-if="store.error" class="text-red-600 mb-6 text-center font-semibold">
+      {{ store.error }}
+    </p>
+
+    <div class="bg-white rounded-2xl shadow-lg overflow-x-auto">
+      <UsuarioList
+        :maquina="store.maquinas"
         @edit="edit"
         @remove="store.removeMaquina"
       />
     </div>
   </section>
+
 </template>
 
 <script setup>
@@ -41,29 +78,38 @@ import { ref, onMounted } from "vue";
 import { useMaquinaStore } from "../stores/maquina";
 import MaquinaForm from "../components/MaquinaForm.vue";
 import MaquinaList from "../components/MaquinaList.vue";
+import UsuarioForm from "../components/UsuarioForm.vue";
 
-const store = useMaquinaStore();
-const editing = ref(null);
+const storeM = useMaquinaStore();
+const editingM = ref(null);
 
 onMounted(() => {
-  store.fetchMaquina();
+  storeM.fetchMaquina();
 });
 
-function edit(maquina) {
-  editing.value = { ...maquina };
+function editM(maquina) {
+  editingM.value = { ...maquina };
 }
 
-function cancelEdit() {
-  editing.value = null;
+function cancelEditM() {
+  editingM.value = null;
 }
 
-// function addMaquina() {
-//   if (newMaquina.value.name && newMaquina.value.type && newMaquina.value.status) {
-//     store.addMaquina(newMaquina.value);
-//     newMaquina.value = { name: '', type: '', status: '' }; 
-//   } else {
-//     alert('Por favor, preencha todos os campos');
-//   }
-// }
+
+const storeU = useUsuarioStore();
+const editingU = ref(null);
+
+onMounted(() => {
+  storeU.fetchUsuario();
+});
+
+function edit(usuario) {
+  editingU.value = { ...usuario };
+}
+
+function cancelEditU() {
+  editingU.value = null;
+}
+
 </script>
 
